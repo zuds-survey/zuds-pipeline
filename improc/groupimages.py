0efb02d82944c5cd577d2a87dfc0caf9e9e5bcc3
Danny Goldstein <dgold@berkeley.edu>
2018-10-12 21:57:37 -0700

    bug fixes in group images

--- improc/groupimages.py
+++ improc/groupimages.py
@@ -24,10 +24,12 @@ def make_directory(topdir, dirname, framenames, clobber):
     refdir = os.path.join(topdir, dirname)
     if os.path.exists(refdir):
         action = 'Overwriting' if clobber else 'Not overwriting'
-        warnings.warn('Template directory %s already exists. %s...' % (refdir, action), UserWarning)
+        warnings.warn('Directory "%s" already exists. %s...' % (refdir, action), UserWarning)
         if clobber:
             shutil.rmtree(refdir)
             os.mkdir(refdir)
+    else:
+        os.mkdir(refdir)
     for frame in framenames:
         shutil.copy(frame, refdir)
 
@@ -36,59 +38,94 @@ def img_in_range(image, range_low, range_high):
     time = pd.datetime(fits.open(image)[0].header['SHUTOPEN'])
     return range_low <= time < range_high
 
+def get_seeing(image):
+    seeing = float(fits.open(image)[0].header['SEEING'])
+    return seeing
+
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--reference-frames', required=True, help='List of the frames to build '
-                        'a reference with (one per line).', fromfile_prefix_chars='@',
-                        type=str, nargs='+')
-    parser.add_argument('--output-dir', help='Directory in which to write results.', action=ReadableDir,
+    parser.add_argument('--input-frames', required=True, help='List of the frames to coadd.',
+                        nargs=1, dest='frames')
+    parser.add_argument('--output-dir', help='Directory in which to write results.', 
                         dest='outdir', required=False, default='.')
-    parser.add_argument('--rolling', help='Use a rolling window to make the groups.', default=False,
+    parser.add_argument('--associated-frames', required=False, default=None, help='Lists of frames, in '
+                        'the same order as input_frames, that should be grouped with the input frames.',
+                        dest='associated', nargs='*')
+    parser.add_argument('--rolling', help='Use a rolling window to group input frames.', default=False,
                         action='store_true', dest='rolling')
     parser.add_argument('--max-seeing', type=float, default=3., required=False, help='The maximum '
-                        'seeing allowed for a reference frame.')
-    parser.add_argument('scidate_range', required=True, type=pd.datetime, help='Date range for science images.',
-                        nargs=2)
-    parser.add_argument('--window-size', type=int, help='Number of days over which to coadd science images.',
-                        required=False, default=10)
-    parser.add_argument('--sciimg-dir', required=False, default='.', action=ReadableDir, nargs=1,
-                        help='The directory to search for science images.')
+                        'seeing allowed for an input frame.')
+    parser.add_argument('--window-size', type=int, help='Number of days over which to coadd input frames.'
+                        ' If zero (default), then coadd all frames.',
+                        required=False, default=0)
     parser.add_argument('--clobber', required=False, default=False, action='store_true',
                         help='Overwrite existing output directories.')
 
     args = parser.parse_args()
+    with open(args.frames[0], 'r') as f:
+        framenames = [line.strip() for line in f]
 
-    # make the reference directory
-    make_directory(args.outdir, 'template', args.reference_frames, args.clobber)
+    # prune the ones with bad seeing
+    frames = []
+    inds = []
+    for i, img in enumerate(framenames):
+        seeing = get_seeing(img)
+        if seeing <= args.max_seeing:
+            frames.append(img)
+            inds.append(i)
+        else:
+            warnings.warn('Frame %s has seeing=%0.2f (>%0.2f). '
+                          'Skipping...' % (img, seeing, args.max_seeing),
+                          UserWarning)
+    # make the bins
 
-    # make the science image groups, rolling or partitioned
+    if args.window_size > 0:
+        mindate, maxdate = args.daterange
+        if args.rolling:
+            dates = pd.date_range(mindate, maxdate, freq='1D')
+            datebins = []
+            for i, date in enumerate(dates):
+                if i >= len(dates):
+                    continue
+                datebins.append((date, dates[i + args.window_size]))
 
-    # list all the science images
-    sciimgs = glob.glob(args.sciimg_dir)
+        else:
+            datebins = pd.date_range(mindate, maxdate, freq='%dD' % args.window_size)
+            datebins = [(datebins[i], datebins[i + 1]) for i in range(len(datebins) - 1)]
 
-    mindate, maxdate = args.daterange
+        for start, stop in datebins:
+            startstr = start.strftime('%y%m%d')
+            stopstr = stop.strftime('%y%m%d')
 
-    if args.rolling:
-        dates = pd.date_range(mindate, maxdate, freq='1D')
-        datebins = []
-        for i, date in enumerate(dates):
-            if i >= len(dates):
-                continue
-            datebins.append((date, dates[i + args.window_size]))
+            dirname = '%s-%s' % (startstr, stopstr)
 
+            myframes = []
+            myinds = []
+            for im, ind in zip(frames, inds):
+                if img_in_range(im, start, stop):
+                    myframes.append(img)
+                    myinds.append(ind)
+
+            for l in args.associated:
+                with open(l, 'r') as f:
+                    aframes = [line.strip() for i, line in enumerate(f) if i in myinds]
+                    if len(aframes) != len(frames):
+                        raise ValueError('Length of associated frame list "%s" must be the same '
+                                         'as number of input frames.' % l)
+                    myframes.extend(aframes)
+            
+            make_directory(args.outdir, dirname, myframes, args.clobber)
+            
     else:
-        datebins = pd.date_range(mindate, maxdate, freq='%dD' % args.window_size)
-        datebins = [(datebins[i], datebins[i + 1]) for i in range(len(datebins) - 1)]
-
-    for start, stop in datebins:
-        startstr = start.strftime('%y%m%d')
-        stopstr = stop.strftime('%y%m%d')
-
-        dirname = '%s-%s' % (startstr, stopstr)
-
-        framegen = filter(lambda im: img_in_range(im, start, stop), sciimgs)
-        frames = list(framegen)
-
-        make_directory(args.outdir, dirname, frames, args.clobber)
+        # just do one big coadd
+        for l in args.associated:
+            with open(l, 'r') as f:
+                aframes = [line.strip() for i, line in enumerate(f) if i in inds]
+                if len(aframes) != len(frames):
+                    raise ValueError('Length of associated frame list "%s" must be the same '
+                                     'as number of input frames.' % l)
+                frames.extend(aframes)
+        make_directory(args.outdir, '', frames, args.clobber)
+    
