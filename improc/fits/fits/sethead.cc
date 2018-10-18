f70166f509f8443bcf4814e488b46221f44206fb
Danny Goldstein <dgold@berkeley.edu>
2018-10-18 13:41:20 -0700

    datatype stuff

--- improc/fits/fits/sethead.cc
+++ improc/fits/fits/sethead.cc
@@ -11,6 +11,14 @@ void updateheader(char* fname, char* key, int datatype, void* value){
     char card[FLEN_CARD];   /* standard string lengths defined in fitsioc.h */
     char comment[100];
 
+    if (datatype == TFLOAT){
+        float* myval = (float *)value;
+    } else if ( datatype == TSTRING ) {
+        char** myval = (char**)value;
+    } else if (datatype == TINT) {
+        int* myval = (int *) value;
+    }
+
     status = 0;
 
     if ( fits_open_file(&fptr, fname, READONLY, &status) )
@@ -19,7 +27,7 @@ void updateheader(char* fname, char* key, int datatype, void* value){
     if ( fits_movabs_hdu(fptr, 1, &hdutype, &status) )
          printerror( status );
 
-    if ( fits_update_key(fptr, datatype, key, value, comment, &status) )
+    if ( fits_update_key(fptr, datatype, key, myval, comment, &status) )
          printerror( status );
 
     return;
