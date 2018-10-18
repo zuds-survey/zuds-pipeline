41a45c3a2c6bb2b4f0c2f47fbc6bcfe43713d8ce
Danny Goldstein <dgold@berkeley.edu>
2018-10-18 13:52:14 -0700

    more cython interface

--- improc/fits/fits/cfits.pyx
+++ improc/fits/fits/cfits.pyx
@@ -1,17 +1,47 @@
 # cython: c_string_type=str, c_string_encoding=ascii
 import cython
 
-__all__ = ['read_header_string', 'read_header_float', 'read_header_int']
+__all__ = ['read_header_string', 'read_header_float', 'read_header_int',
+           'update_header_string', 'update_header_float', 'update_header_int']
 
 
 cdef extern from "gethead.hh":
     void readheader(char* fname, char* key, int datatype, void* value) except +
 
+cdef extern from "sethead.hh":
+    void updateheader(char* fname, char* key, int datatype, void* value) except +
 
 cdef extern from "fitsio.h":
     int TFLOAT
     int TSTRING
     int TINT
+
+
+@cython.embedsignature(True)
+def update_header_string(fname, key, value):
+    cdef:
+        char* string = value;
+        void* val = <void *> &string
+
+    updateheader(fname, key, TSTRING, val)
+
+
+@cython.embedsignature(True)
+def update_header_float(fname, key, value):
+    cdef:
+        float number = value;
+        void* val = <void *> &number
+
+    updateheader(fname, key, TFLOAT, val)
+
+
+@cython.embedsignature(True)
+def update_header_string(fname, key, value):
+    cdef:
+        int i = value;
+        void* val = <void *> i
+
+    updateheader(fname, key, TINT, val)
 
 
 @cython.embedsignature(True)
