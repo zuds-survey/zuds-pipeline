7b62c219554653020cfad87a34ec54080ccc2202
Danny Goldstein <dgold@berkeley.edu>
2018-10-27 11:57:11 -0400

    fix naxis format type

--- stackpipe/makesub.py
+++ stackpipe/makesub.py
@@ -131,7 +131,7 @@ if __name__ == '__main__':
 
         # Merge header files
         with open(refremaphead, 'w') as f:
-            f.write('NAXIS       %d\n' % str(naxis))
+            f.write('NAXIS       %d\n' % naxis)
             with open(newhead, 'r') as nh:
                 f.write(nh.read())
 
