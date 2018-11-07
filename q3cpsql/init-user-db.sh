262f5f87e2a1648c3d74ecb51bbfbb4e27e21939
Danny Goldstein <dgold@berkeley.edu>
2018-11-06 21:15:52 -0800

    add a test database

---
+++ q3cpsql/init-user-db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+CREATE USER ztfcoadd_admin;
+CREATE DATABASE ztfcoadd;
+GRANT ALL PRIVILEGES ON DATABASE ztfcoadd TO ztfcoadd_admin;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "ztfcoadd" <<-EOSQL
+CREATE EXTENSION q3c;
+EOSQL
