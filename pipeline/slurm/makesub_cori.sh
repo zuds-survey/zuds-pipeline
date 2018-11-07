91358b4ea5b013a6fdf24dec7f47fc1d2b87db7f
Danny Goldstein <dgold@berkeley.edu>
2018-11-06 19:18:09 -0800

    editing batch submission scripts

--- pipeline/slurm/makesub_cori.sh
+++ pipeline/slurm/makesub_cori.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -J diffem
+#SBATCH -J stckdiff
 #SBATCH -t 00:30:00
 #SBATCH -L SCRATCH
 #SBATCH -A ***REMOVED***
@@ -8,7 +8,10 @@
 #SBATCH --partition=realtime
 #SBATCH --mail-user=dgold@berkeley.edu
 #SBATCH --image=registry.services.nersc.gov/dgold/improc:latest
+#SBATCH --dependency={dlist:s}
 
 news=$1
-refs=$2
+template=$2
+
+
 srun -n 64 shifter python /lensgrinder/pipeline/bin/makesub.py --science-images=${news} --templates=${refs}
