520e33d7d8e7063ea99263fc2471cc6da363c823
Danny Goldstein <dgold@berkeley.edu>
2019-01-05 22:23:36 -0800

    trying again
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- pipeline/slurm/makecoadd_cori.sh
+++ pipeline/slurm/makecoadd_cori.sh
@@ -11,7 +11,7 @@
 #SBATCH --dependency=afterok:DLIST
 #SBATCH -C haswell
 #SBATCH --exclusive
-#SBATCH --volume=/global/homes/d/dgold:/home/desi;/global/cscratch1/sd/dgold/lensgrinder/pipeline/astromatic:/pipeline/config;/global/cscratch1/sd/dgold/lensgrinder/pipeline:/pipeline
+#SBATCH --volume=/global/homes/d/dgold:/home/desi;/global/cscratch1/sd/dgold/lensgrinder/pipeline:/pipeline;/global/cscratch1/sd/dgold/lensgrinder/pipeline/astromatic:/pipeline/config
 #SBATCH -o $4/slurm-%A.out
 
 export OMP_NUM_THREADS=1
