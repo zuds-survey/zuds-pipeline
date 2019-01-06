598a86ec241e4c9a9b5206b6d2375f6a8e7c0199
Danny Goldstein <dgold@berkeley.edu>
2019-01-06 14:38:13 -0800

    mount stamps
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- pipeline/slurm/makecoaddsub_cori.sh
+++ pipeline/slurm/makecoaddsub_cori.sh
@@ -10,7 +10,7 @@
 #SBATCH --image=registry.services.nersc.gov/dgold/improc:latest
 #SBATCH --dependency=afterok:DLIST
 #SBATCH -C haswell
-#SBATCH --volume=/global/homes/d/dgold:/home/desi;/global/cscratch1/sd/dgold/lensgrinder/pipeline:/pipeline;/global/cscratch1/sd/dgold/lensgrinder/pipeline/astromatic:/config
+#SBATCH --volume=/global/homes/d/dgold:/home/desi;/global/cscratch1/sd/dgold/lensgrinder/pipeline:/pipeline;/global/cscratch1/sd/dgold/lensgrinder/pipeline/astromatic:/config;/global/cscratch1/sd/dgold/ztfcoadd/stamps:/stamps
 #SBATCH -o $4/slurm-%A.out
 #SBATCH --exclusive
 
