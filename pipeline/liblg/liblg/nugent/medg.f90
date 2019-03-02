51fad67f0edb8257b2cfd6f57787b09bec991498
Danny Goldstein <dgold@berkeley.edu>
2019-03-01 18:20:47 -0800

    5 sigma limiting mag not 3 sigma
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- pipeline/liblg/liblg/nugent/medg.f90
+++ pipeline/liblg/liblg/nugent/medg.f90
@@ -1,5 +1,5 @@
 ! -*- f90 -*-
-!- 
+!-
 !- File    : medg.f
 !- ------------------------
 !- Original by PN
@@ -17,7 +17,7 @@
 !
 
       integer :: nc, nr, i, j, stat
-      real, allocatable :: image(:,:) 
+      real, allocatable :: image(:,:)
       real :: med, var, zp, see, lmt, pix
       character (len = 700) :: filenm
       character (len = 72) :: comment
@@ -26,7 +26,7 @@
       call get_header(trim(filenm),'NAXIS2',nr,comment)
 
       allocate(image(nc,nr), stat=stat)
-      
+
       call get_image(trim(filenm),image,nc,nr)
 
       call get_header(trim(filenm),'MAGZP',zp,comment)
@@ -36,12 +36,12 @@
       image = abs(image - med)
       call get_med_image(image, nc, nr, var)
 
-      lmt = -2.5*log10(3.0*sqrt(3.14159*see*see)*var*1.48) + zp
+      lmt = -2.5*log10(5.0*sqrt(3.14159*see*see)*var*1.48) + zp
 
 !-      print *, lmt, zp, see, pix
 
       call put_header(trim(filenm),'MEDSKY',med,'Median sky in cts')
       call put_header(trim(filenm),'SKYSIG',var,'Median skysig in cts')
-      call put_header(trim(filenm),'LMT_MG',lmt,'3-sig limiting mag')
+      call put_header(trim(filenm),'LMT_MG',lmt,'5-sig limiting mag')
 
       end subroutine medg
