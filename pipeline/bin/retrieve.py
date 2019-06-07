21edd9090a85c3d594e955e22d2404c9587e6224
Danny Goldstein <dgold@berkeley.edu>
2019-06-06 21:44:05 -0700

    dont use bad images that will mess things up
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- pipeline/bin/retrieve.py
+++ pipeline/bin/retrieve.py
@@ -123,7 +123,7 @@ def retrieve_images(whereclause, exclude_masks=False, job_script_destination=Non
     hpssdb = HPSSDB()
 
     # this is the query to get the image paths
-    query = f'SELECT * FROM IMAGE WHERE HPSS_SCI_PATH IS NOT NULL AND {whereclause}'
+    query = f'SELECT * FROM IMAGE WHERE HPSS_SCI_PATH IS NOT NULL AND {whereclause} AND SEEING < 3 AND MAGLIMIT > 18.5'
     metatable = pd.read_sql(query, hpssdb.engine)
     df = metatable[['path', 'hpss_sci_path', 'hpss_mask_path']]
 
