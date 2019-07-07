ef070ba1c396cb3cd741e0acbb607752376cef0e
ztfproc Pseudo User <ztfproc@cori10.nersc.gov>
2019-07-06 23:47:23 -0700

    installed update into crontab

--- pipeline/bin/update.py
+++ pipeline/bin/update.py
@@ -1,8 +1,10 @@
+import os
 import db
 import numpy as np
 import pandas as pd
 from datetime import datetime
 from ztfquery import query
+
 
 QUERY_WINDOWSIZE = 30 # days
 WHERECLAUSE = ''
@@ -15,6 +17,8 @@ if __name__ == '__main__':
     db.init_db(**cfg['database'])
 
     zquery = query.ZTFQuery()
+    
+    start = datetime.now()
 
     # get the maximum nid
     max_nid = db.DBSession().query(db.sa.func.max(db.Image.nid)).first()[0]
@@ -28,27 +32,38 @@ if __name__ == '__main__':
     mod = nid_diff % QUERY_WINDOWSIZE
 
     n_chunks = quotient if mod == 0 else quotient + 1
+    
+    print('querying')
 
     for i in range(n_chunks):
         zquery.load_metadata(kind='sci', sql_query=WHERECLAUSE + (' AND ' if WHERECLAUSE != '' else '') +
-                             f'NID BETWEEN {max_nid + QUERY_WINDOWSIZE * i + 1} AND '
-                             f'{max_nid + QUERY_WINDOWSIZE * (i + 1)}')
+                             f'NID BETWEEN {max_nid + QUERY_WINDOWSIZE * i} AND '
+                             f'{max_nid + QUERY_WINDOWSIZE * (i + 1)} AND IPAC_GID > 0',
+                             auth=[os.getenv('IPAC_USERNAME'),
+                                   os.getenv('IPAC_PASSWORD')])
         metatable = zquery.metatable
         metatables.append(metatable)
 
     metatable = pd.concat(metatables)
     current_paths = db.DBSession().query(db.Image.path).all()
+    print(f'pulled {len(metatable)} images')
 
     # dont need this
     del metatable['imgtype']
     del metatable['ipac_pub_date']
 
-    meta_images = [db.Image(**row.to_dict()) for _, row in metatable.iterrows() if _ < 10]
+    meta_images = [db.Image(**row.to_dict()) for _, row in metatable.iterrows()]
     basenames = [i.ipac_path('sciimg.fits').split('/')[-1] for i in meta_images]
 
-    indices = np.nonzero(~np.in1d(basenames, current_paths, assume_unique=True))
+    indices = np.nonzero(~np.in1d(basenames, current_paths, assume_unique=True))[0]
+    
+    print(f'uploading images')
 
     for index in indices:
         meta_images[index].path = basenames[index]
         db.DBSession().add(meta_images[index])
     db.DBSession().commit()
+    
+    end = datetime.now()
+
+    print(f'done in {end - start}')
