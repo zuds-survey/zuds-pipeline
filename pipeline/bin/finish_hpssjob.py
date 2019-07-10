d0679b8a0c120ebaf4a5e023ece2edc0955bf0da
Danny Goldstein <dgold@berkeley.edu>
2019-07-09 22:41:16 -0700

    add method for dealing with HPSS jobs
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

---
+++ pipeline/bin/finish_hpssjob.py
@@ -0,0 +1,14 @@
+import db
+import sys
+
+if __name__ == '__main__':
+
+    env, cfg = db.load_env()
+    db.init_db(**cfg['database'])
+
+    jobid = int(sys.argv[1])
+
+    hpssjob = db.DBSession().query(db.HPSSJob).get(jobid)
+    hpssjob.status = True
+    db.DBSession().add(hpssjob)
+    db.DBSession().commit()
