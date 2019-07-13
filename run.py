41f2dc0367fbf1accadab4295edfbbe68594effc
Danny Goldstein <dgold@berkeley.edu>
2019-07-13 19:58:09 +0000

    do single epoch subtractions

--- run.py
+++ run.py
@@ -205,3 +205,9 @@ if __name__ == '__main__':
                                                 rolling=rolling, coadd_windowsize=coadd_windowsize,
                                                 batch_size=batch_size, job_script_destination=jobscripts,
                                                 log_destination=logs, frame_destination=framepath, task_name=task_name)
+
+        if coadd_windowsize > 0:
+            sub_dependencies = submit_coaddsub(template_dependencies, variance_dependencies, remaining_images, ref,
+                                               rolling=rolling, coadd_windowsize=0,
+                                               batch_size=batch_size, job_script_destination=jobscripts,
+                                               log_destination=logs, frame_destination=framepath, task_name=task_name)
