6ee33edb685d3b332ed05c8027c560b1507fca98
Danny Goldstein <dgold@berkeley.edu>
2019-07-15 13:27:06 -0700

    try exporting environment variables
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- pipeline/slurm/single_coaddsub.sh
+++ pipeline/slurm/single_coaddsub.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-news="$1"
-cats="$2"
-coadd="$3"
-template="$4"
-subid="$5"
-coaddid="$6"
-sub="$7"
+export news="$1"
+export cats="$2"
+export coadd="$3"
+export template="$4"
+export subid="$5"
+export coaddid="$6"
+export sub="$7"
 
 python /pipeline/bin/makecoadd.py --input-frames $news --input-catalogs $cats --outfile-path=$coadd && \
 python /pipeline/bin/log_image.py $coadd $coaddid Stack
