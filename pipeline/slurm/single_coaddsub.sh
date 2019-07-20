00eeeade40089db2f75d47dffe88201ae7615cde
Danny Goldstein <dgold@berkeley.edu>
2019-07-20 01:08:04 -0700

    check retcode on movedeps

--- pipeline/slurm/single_coaddsub.sh
+++ pipeline/slurm/single_coaddsub.sh
@@ -10,6 +10,8 @@ export sub="$7"
 
 python /pipeline/bin/movedeps.py $news $coadd
 
+if [ $? -eq 0 ]; then 
+
 echo "python /pipeline/bin/makecoadd.py --input-frames $news --input-catalogs $cats --outfile-path=$coadd && \
 python /pipeline/bin/log_image.py $coadd $coaddid Stack"
 
@@ -25,3 +27,5 @@ python /pipeline/bin/makesub.py --science-frames $coadd --templates $template &&
 python /pipeline/bin/log_image.py $sub $subid MultiEpochSubtraction
 
 fi
+
+fi 
