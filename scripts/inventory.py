0db408c0e766e4211598eafbf917bb450cc7e5e1
Danny Goldstein <dgold@berkeley.edu>
2019-11-16 15:46:31 -0800

    create db objects and better manage tempoarary dirs
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- scripts/inventory.py
+++ scripts/inventory.py
@@ -3,6 +3,7 @@ import db
 import subprocess
 import tarfile
 import uuid
+import shutil
 from pathlib import Path
 from mpi import get_my_share_of_work
 
@@ -20,9 +21,11 @@ if __name__ == '__main__':
     db.init_db()
     my_files = get_my_share_of_work('tars')
     tmpdir = Path('/global/cscratch1/sd/dgold/inventory')
+    olddir = os.getcwd()
 
     for file in my_files:
         mydir = tmpdir / uuid.uuid4().hex
+        mydir.mkdir()
         os.chdir(mydir)
         cmd = f'/usr/common/mss/bin/hsi get {file}'
         try:
@@ -31,6 +34,9 @@ if __name__ == '__main__':
             continue
         tar = tarfile.open(os.path.basename(file))
         tar.extractall()
+
+        tapearchive = db.TapeArchive(id=file)
+
         for member in tar:
             basename = os.path.basename(member.name)
             obj = db.DBSession().query(db.PipelineProduct).filter(
@@ -38,9 +44,14 @@ if __name__ == '__main__':
             if obj is None:
                 obj = object_from_filename(basename)
 
-            db.DBSession().add(obj)
+            copy = db.TapeCopy(product=obj, archive=tapearchive)
+            db.DBSession().add(copy)
+
         tar.close()
-        os.remove(os.path.basename(file))
         db.DBSession().commit()
+        os.chdir(olddir)
+        shutil.rmtree(mydir)
+
+
 
 
