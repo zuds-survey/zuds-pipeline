19602fac85a3522020083280e4e6398c3899e2fa
Danny Goldstein <dgold@berkeley.edu>
2019-11-19 15:41:26 -0800

    parallelize loaddb.py
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- scripts/loaddb.py
+++ scripts/loaddb.py
@@ -1,13 +1,15 @@
 import db
 db.init_db()  # zuds2
-
+import numpy as np
 import pandas as pd
 
-# this may need to be parallelized
-df = pd.read_csv('/global/cscratch1/sd/dgold/image_tiny.csv')
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
-archives = {}
-# first group by "hpss_sci_path"
+# this may need to be parallelized
+df = pd.read_csv('/global/cscratch1/sd/dgold/image.csv')
 
 fid_map = {
     'zg': 1,
@@ -15,58 +17,79 @@ fid_map = {
     'zi': 3
 }
 
-archives = {}
+
+if rank == 0:
+    archives = pd.concat(df['hpss_sci_path'], df['hpss_mask_path']).unique()
+    tarchs = []
+    arch_map = {}
+    for archive in archives:
+        if archive is None:
+            continue
+        dbarch = db.TapeArchive(id=archive)
+        db.DBSession().add(dbarch)
+        tarchs.append(dbarch)
+    db.DBSession().commit()
+    for dbarch in tarchs:
+        arch_map[dbarch.id] = dbarch
+    subframes = np.array_split(df, size)
+
+else:
+    subframes = None
+    arch_map = None
+
+arch_map = comm.bcast(arch_map, root=0)
+myframes = comm.scatter(subframes, root=0)
 
 # load in chunks of 50k
-for i, row in df.iterrows():
+for i, row in myframes.iterrows():
     poly_dict = {}
-    for key in ['ra', 'dec', 'ra1', 'dec1', 'ra2', 'dec2', 'ra3', 'dec3', 'ra4', 'dec4']:
+    for key in ['ra', 'dec', 'ra1', 'dec1', 'ra2', 'dec2', 'ra3',
+                'dec3', 'ra4', 'dec4']:
         poly_dict[key] = row[key]
 
     sci = db.ScienceImage(basename=row['path'], filtercode=row['filtercode'],
-                          qid=row['qid'], field=row['field'], ccdid=row['ccdid'],
+                          qid=row['qid'], field=row['field'],
+                          ccdid=row['ccdid'],
                           obsjd=row['obsjd'], infobits=row['infobits'],
                           pid=row['pid'], nid=row['nid'],
-                          expid=row['expid'], itid=row['itid'], obsdate=row['obsdate'],
+                          expid=row['expid'], itid=row['itid'],
+                          obsdate=row['obsdate'],
                           seeing=row['seeing'], airmass=row['airmass'],
-                          moonillf=row['moonillf'], moonesb=row['moonesb'], maglimit=row['maglimit'],
-                          crpix1=row['crpix1'], crpix2=row['crpix2'], crval1=row['crval1'],
-                          crval2=row['crval2'], cd11=row['cd11'], cd12=row['cd12'], cd21=row['cd21'],
-                          cd22=row['cd22'], ipac_gid=row['ipac_gid'], imgtypecode=row['imgtypecode'],
-                          exptime=row['exptime'], filefracday=row['filefracday'],
+                          moonillf=row['moonillf'], moonesb=row['moonesb'],
+                          maglimit=row['maglimit'],
+                          crpix1=row['crpix1'], crpix2=row['crpix2'],
+                          crval1=row['crval1'],
+                          crval2=row['crval2'], cd11=row['cd11'],
+                          cd12=row['cd12'],
+                          cd21=row['cd21'],
+                          cd22=row['cd22'], ipac_gid=row['ipac_gid'],
+                          imgtypecode=row['imgtypecode'],
+                          exptime=row['exptime'],
+                          filefracday=row['filefracday'],
                           fid=fid_map[row['filtercode']], **poly_dict)
 
-    msk = db.MaskImage(parent_image=sci, basename=row['path'].replace('sciimg', 'mskimg'),
-                       field=row['field'], ccdid=row['ccdid'], fid=fid_map[row['filtercode']],
+    msk = db.MaskImage(parent_image=sci,
+                       basename=row['path'].replace('sciimg', 'mskimg'),
+                       field=row['field'],
+                       ccdid=row['ccdid'], fid=fid_map[row['filtercode']],
                        qid=row['qid'], **poly_dict)
-
 
     hpss_sci_path = row['hpss_sci_path']
     if hpss_sci_path is not None:
-        try:
-            scitar = archives[hpss_sci_path]
-        except KeyError:
-            scitar = db.TapeArchive(id=hpss_sci_path)
-            archives[hpss_sci_path] = scitar
-            db.DBSession().add(scitar)
-
+        scitar = arch_map[hpss_sci_path]
         scicopy = db.TapeCopy(archive=scitar, product=sci)
         db.DBSession().add(scicopy)
 
     hpss_mask_path = row['hpss_mask_path']
     if hpss_mask_path is not None:
-        try:
-            masktar = archives[hpss_mask_path]
-        except KeyError:
-            masktar = db.TapeArchive(id=hpss_mask_path)
-            archives[hpss_mask_path] = masktar
-            db.DBSession().add(masktar)
+        masktar = arch_map[hpss_mask_path]
         maskcopy = db.TapeCopy(archive=masktar, product=msk)
+        db.DBSession().add(maskcopy)
 
     db.DBSession().add(msk)
     db.DBSession().add(sci)
 
-    if (i != 0 and i % 50000 == 0) or (i == len(df) - 1):
+    if (i != 0 and i % 50000 == 0) or (i == len(myframes) - 1):
         db.DBSession().commit()
-        
+
 
