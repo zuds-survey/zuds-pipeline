0eabe934c902195d0e00b80f530a08e96fd4793c
Danny Goldstein <dgold@berkeley.edu>
2019-11-22 20:23:58 -0800

    add a script to make all the weight maps and the rms maps
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

---
+++ scripts/makeweight.py
@@ -0,0 +1,20 @@
+import db
+import sys
+import mpi
+
+db.init_db()
+
+__author__ = 'Danny Goldstein <danny@caltech.edu>'
+__whatami__ = 'Make the weight maps for ZUDS.'
+
+infile = sys.argv[0]  # file listing all the images to build weight maps for
+
+# get the work
+sci_fns = mpi.get_my_share_of_work(infile)
+
+# load the objects into memory
+for fn in sci_fns:
+    sci = db.ScienceImage.from_file(fn)
+    sci.weight_image.save()
+    sci.rms_image.save()
+
