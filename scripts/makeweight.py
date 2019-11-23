6b4a180a529e04a5117d3f68ea08559e614c118a
Danny Goldstein <dgold@berkeley.edu>
2019-11-22 21:12:25 -0800

    use chdir
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- scripts/makeweight.py
+++ scripts/makeweight.py
@@ -1,4 +1,5 @@
 import db
+import os
 import sys
 import mpi
 
@@ -12,17 +13,22 @@ infile = sys.argv[1]  # file listing all the images to build weight maps for
 # get the work
 sci_fns = mpi.get_my_share_of_work(infile)
 
+cwd = os.getcwd()
+
 # load the objects into memory
 for fn in sci_fns:
+
     # this line assumes that .mskimg.fits is in the same directory
     sci = db.ScienceImage.from_file(fn)
+    os.chdir(os.path.dirname(fn))
     sci.weight_image.save()
     sci.rms_image.save()
+    os.chdir(cwd)
 
-    # clean up 
+    # clean up
     sci.mask_image.unmap()
     sci.unmap()
     del sci.mask_image
     del sci
-    
+
 
