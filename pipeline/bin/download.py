614b003a68dc6e878f7eabbf5ab4e60b09361c6f
Danny Goldstein <dgold@berkeley.edu>
2019-12-04 16:11:24 -0800

    remove sort order
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- pipeline/bin/download.py
+++ pipeline/bin/download.py
@@ -186,7 +186,6 @@ if __name__ == '__main__':
             ),
             db.ZTFFile.field.in_(ZUDS_FIELDS)
         ).with_for_update(skip_locked=True).order_by(
-            sort_order,
             db.ZTFFile.field,
             db.ZTFFile.ccdid,
             db.ZTFFile.qid,
