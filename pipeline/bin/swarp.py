b733eadf829bd81bc9f4bc52de1371a1f815c7a1
Danny Goldstein <dgold@berkeley.edu>
2020-01-03 11:26:09 -0800

    explicitly load things in make_coadd
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- pipeline/bin/swarp.py
+++ pipeline/bin/swarp.py
@@ -255,10 +255,18 @@ def run_coadd(cls, images, outname, mskoutname, addbkg=True,
 
 
     # load the result
-    coadd = cls.from_file(outname)
+    coadd = cls.get_by_basename(os.path.basename(outname))
+    coadd.map_to_local_file(outname)
+
+    if coadd.catalog is not None:
+        coadd.catalog.clear()
+
+    coaddweightname = outname.replace('.fits', '.weight.fits')
+    coadd._weightimg = db.FITSImage.from_file(coaddweightname)
+
     coaddmask = db.MaskImage.from_file(mskoutname)
-    coaddweight = db.FITSImage.from_file(mskoutweightname)
-    coaddmask.update_from_weight_map(coaddweight)
+    coaddmaskweight = db.FITSImage.from_file(mskoutweightname)
+    coaddmask.update_from_weight_map(coaddmaskweight)
 
     # keep a record of the images that went into the coadd
     coadd.input_images = images.tolist()
