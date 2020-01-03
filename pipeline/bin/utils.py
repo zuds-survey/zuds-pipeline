e0e4ba7b2a3f23c899f5efb02a0a467d22ab857e
Danny Goldstein <dgold@berkeley.edu>
2020-01-03 12:50:41 -0800

    i want  subtractions produced via the same command to be identical !!!!
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- pipeline/bin/utils.py
+++ pipeline/bin/utils.py
@@ -7,7 +7,7 @@ def initialize_directory(directory):
     directory.mkdir(parents=True, exist_ok=True)
 
 
-def quick_background_estimate(image, nsamp=750000, mask_image=None):
+def quick_background_estimate(image, nsamp=None, mask_image=None):
     # only requires that image.data and image.mask_image be defined
     # we only need a quick estimate of the bkg.
     # so we mask out any pixels where the MASK value is non zero.
@@ -16,7 +16,9 @@ def quick_background_estimate(image, nsamp=750000, mask_image=None):
         mask_image = image.mask_image
 
     bkgpix = image.data[mask_image.data == 0]
-    bkgpix = np.random.choice(bkgpix, size=nsamp)
+
+    if nsamp is not None:
+        bkgpix = np.random.choice(bkgpix, size=nsamp)
 
     bkg = np.median(bkgpix)
 
