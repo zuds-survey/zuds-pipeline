ef2cb381017265fc8d5eeda32e48ac452fa539a1
Danny Goldstein <dgold@berkeley.edu>
2020-01-07 14:48:18 -0800

    add code to run in slurm jobs
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

---
+++ scripts/donightly.py
@@ -0,0 +1,37 @@
+import db
+import sys
+import mpi
+import dosub
+import makesources
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser()
+    parser.add_argument('filenames', nargs='*', type=str)
+    parser.add_argument('refvers', type=str)
+    args = parser.parse_args()
+
+    filenames = args.filenames
+    refvers = args.refvers
+
+    # subclass = db.MultiEpochSubtraction
+    # sciclass = db.ScienceCoadd
+
+    subclass = db.SingleEpochSubtraction
+    sciclass = db.ScienceImage
+
+    # get the work
+    imgs = mpi.get_my_share_of_work(filenames, reader=lambda x: x)
+    for fn in imgs:
+
+        # commits
+        detections, sub = dosub.do_one(fn, sciclass, subclass, refvers)
+        for d in detections:
+            # each call commits
+            makesources.associate(d.id, do_historical_phot=True)
+
+        # requires manual commit
+        sub.force_photometry(sub.unphotometered_sources,
+                             assume_background_subtracted=True)
+        db.DBSession().commit()
