a5e52f7be730751c4cf178d82038a3acdd088da2
Danny Goldstein <dgold@berkeley.edu>
2020-01-12 15:18:13 -0800

    try new controller
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- scripts/controller.py
+++ scripts/controller.py
@@ -59,10 +59,7 @@ def submit_job(images):
 
     os.chdir(scriptname.parent)
 
-    copies = [
-        list(filter(lambda x: isinstance(x, db.HTTPArchiveCopy),
-                    image.copies))[0].archive_path for image in images
-    ]
+    copies = [i[0] for i in images]
     inname = f'{scriptname}'.replace('.sh', '.in')
     with open(inname, 'w') as f:
         f.write('\n'.join(copies) + '\n')
@@ -112,6 +109,12 @@ HDF5_USE_FILE_LOCKING=FALSE srun -n 64 -c1 --cpu_bind=cores shifter python $HOME
 if __name__ == '__main__':
     while True:
 
+        # look for failed jobs and mark them
+        currently_processing = db.DBSession().query(
+            db.Job
+        ).filter(db.Job.status == 'processing')
+
+        # get the slurm jobs and their statuses
 
         try:
             job_statuses = get_job_statuses()
@@ -121,47 +124,62 @@ if __name__ == '__main__':
             print(f'continuing...', flush=True)
             continue
 
-        imq = db.DBSession().query(
-            db.HTTPArchiveCopy.archive_path,
-        ).select_from(db.ScienceImage).join(
-            db.HTTPArchiveCopy
-        ).join(
-            # make sure it has a reference Image
-            db.ReferenceImage,
-            db.sa.and_(
-                db.ReferenceImage.field == db.ScienceImage.field,
-                db.ReferenceImage.ccdid == db.ScienceImage.ccdid,
-                db.ReferenceImage.qid == db.ScienceImage.qid,
-                db.ReferenceImage.fid == db.ScienceImage.fid
-            )
-        ).outerjoin(
-            db.SingleEpochSubtraction,
-            db.SingleEpochSubtraction.target_image_id == db.ScienceImage.id
-        ).outerjoin(
-            db.FailedSubtraction,
-            db.FailedSubtraction.target_image_id == db.ScienceImage.id
-        ).filter(
-            db.SingleEpochSubtraction.id == None,
-            db.ReferenceImage.version == 'zuds4',
-            db.FailedSubtraction.id == None,
-            db.ScienceImage.field.in_(
-                ZUDS_FIELDS
-            ),
-        ).with_for_update(
-            skip_locked=True, of=db.ScienceImage.__table__
-        )
+        for job in currently_processing:
+            if job.slurm_id not in list(map(str, job_statuses['JOBID'].tolist())):
+                job.status = 'done'
+                db.DBSession().add(job)
+        db.DBSession().commit()
 
-        images = imq.all()
+        imq = '''select final.ap, final.sid from
+                (select dummy.ap, dummy.sid from
+                    (
+                       select h.archive_path as ap , s.id as sid, zf.basename as zbase
+                       from ztffiles zf
+                       join scienceimages s on zf.id = s.id
+                       join ztffilecopies z on z.product_id = s.id
+                       join httparchivecopies h on h.id=z.id
+                       join (
+                           ztffiles z1
+                           join referenceimages r
+                           on z1.id = r.id
+                       ) on (
+                          z1.field = zf.field
+                          and z1.ccdid=zf.ccdid
+                          and z1.qid=zf.qid
+                          and z1.fid=zf.fid
+                       ) where r.version = 'zuds4'
+                       and zf.field  = %d
+                       and s.ipac_gid = 2
+                       and s.seeing < 4
+                       and s.maglimit > 19
+                     ) dummy
+                     left outer join failedsubtractions f
+                     on f.target_image_id = dummy.sid
+                     left outer join singleepochsubtractions ss
+                     on ss.target_image_id = dummy.sid
+                     where ss.id is null and f.id is null order by dummy.zbase desc
+                ) final left outer join (
+                    select calibratableimage_id as cid, job_id as jid
+                    from job_images ji join jobs j on ji.job_id = j.id
+                    where j.status = 'processing'
+                ) disqualifying on final.sid = disqualifying.cid
+                where disqualifying.jid is null
+'''
 
-        if len(images) == 0:
+        results = []
+        for field in ZUDS_FIELDS:
+            db.DBSession().execute(imq % field)
+            results.extend(db.DBSession().fetchall())
+
+        if len(results) == 0:
             print(f'{datetime.datetime.utcnow()}: Nothing to do, trying again...')
             continue
 
 
-        nchunks = len(images) // JOB_SIZE
-        nchunks += 1 if len(images) % JOB_SIZE != 0 else 0
+        nchunks = len(results) // JOB_SIZE
+        nchunks += 1 if len(results) % JOB_SIZE != 0 else 0
 
-        for group in np.array_split(images, nchunks):
+        for group in np.array_split(results, nchunks):
 
             try:
                 slurm_id = submit_job(group.tolist())
@@ -171,7 +189,15 @@ if __name__ == '__main__':
                 print(f'continuing...', flush=True)
                 continue
 
-            job = db.Job(images=group.tolist(), status='processing', slurm_id=slurm_id)
+            job = db.Job(status='processing', slurm_id=slurm_id)
             db.DBSession().add(job)
+            db.DBSession().flush()
+
+            for row in group:
+                ji = db.JobImage(calibratableimage_id=row[1], job_id=job.id)
+                db.DBSession().add(ji)
+
             db.DBSession().commit()
+
+
 
