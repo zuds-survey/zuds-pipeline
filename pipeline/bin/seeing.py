2886544f4c70ab3ee87348dcdf68b859c264c401
Danny Goldstein <dgold@berkeley.edu>
2020-01-21 20:36:58 -0800

    use dr8_database to estimate seeing
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- pipeline/bin/seeing.py
+++ pipeline/bin/seeing.py
@@ -3,54 +3,44 @@ import numpy as np
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 from secrets import get_secret
-import penquins
+import psycopg2
+
+
+def private_logon():
+    """ Log on to the ztfimages database on private """
+    conn = psycopg2.connect(
+            host=get_secret('hpss_dbhost'), user=get_secret('hpss_dbusername'),
+            password=get_secret('hpss_dbpassword'),
+            database=get_secret('olddb'),
+            port=get_secret('hpss_dbport'))
+    cur = conn.cursor()
+    return cur
 
 
 def estimate_seeing(image):
     """Estimate the seeing on an image by comparing its catalog to GAIA stars."""
 
     # connect to kowalski to query gaia
-    username = get_secret('kowalski_username')
-    password = get_secret('kowalski_password')
-    kowalski = penquins.Kowalski(username=username, password=password)
+
     catalog = image.catalog
+    cursor = private_logon()
 
     if catalog is None or not catalog.ismapped:
         catalog = db.PipelineFITSCatalog.from_image(image)
 
+    q = f'''SELECT "RA", "DEC" FROM dr8_north WHERE
+    q3c_radial_query("RA", "DEC", {image.ra}, {image.dec}, 1.2) AND 
+    "PARALLAX" > 0 AND "GAIA_PHOT_G_MEAN_MAG" > 16. 
+    '''
 
-    q = {"query_type": "cone_search",
-         "object_coordinates": {
-             "radec": [(image.ra, image.dec)],
-             "cone_search_radius": "1.2",
-             "cone_search_unit": "deg"
-         },
-         "catalogs": {
-             "Gaia_DR2": {
-                 "filter": {
-                     "parallax": {"$gt": 0.},
-                     "phot_g_mean_mag": {"$gt": 16.}  # only return stars
-                 },
-                 "projection": {
-                     "_id": 1,
-                     "ra": 1,
-                     "dec": 1
-                 }
-             }
-         },
-         "kwargs": {}
-    }
-    result = kowalski.query(q)
-    if result['status'] != 'done':
-        raise ValueError(f'Kowalski Error: {result}')
+    cursor.execute(q)
+    result = cursor.fetchall()
 
-    stars = result['result_data']['Gaia_DR2']
     matchra = []
     matchdec = []
-    for d in stars.values():
-        for dd in d:
-            matchra.append(dd['ra'])
-            matchdec.append(dd['dec'])
+    for d in result:
+        matchra.append(d[0])
+        matchdec.append(d[1])
 
     matchcoord = SkyCoord(matchra, matchdec, unit='deg')
     catcoord = SkyCoord(catalog.data['X_WORLD'], catalog.data['Y_WORLD'], unit='deg')
