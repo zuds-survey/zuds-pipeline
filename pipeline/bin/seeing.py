e3e135dab54ae672ed20ca4db81b9c05749aff55
Danny Goldstein <dgold@berkeley.edu>
2020-02-01 20:56:07 -0800

    go back to old way of calculating seeing on coadds

--- pipeline/bin/seeing.py
+++ pipeline/bin/seeing.py
@@ -3,33 +3,64 @@ import numpy as np
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 from secrets import get_secret
-import psycopg2
-
-
-def private_logon():
-    """ Log on to the ztfimages database on private """
-    conn = psycopg2.connect(
-            host=get_secret('hpss_dbhost'), user=get_secret('hpss_dbusername'),
-            password=get_secret('hpss_dbpassword'),
-            database=get_secret('olddb'),
-            port=get_secret('hpss_dbport'))
-    cur = conn.cursor()
-    return cur
+import penquins
 
 
 def estimate_seeing(image):
     """Estimate the seeing on an image by comparing its catalog to GAIA stars."""
 
     # connect to kowalski to query gaia
-
+    username = get_secret('kowalski_username')
+    password = get_secret('kowalski_password')
+    kowalski = penquins.Kowalski(username=username, password=password)
     catalog = image.catalog
 
     if catalog is None or not catalog.ismapped:
         catalog = db.PipelineFITSCatalog.from_image(image)
 
-    data = catalog.data
+
+    q = {"query_type": "cone_search",
+         "object_coordinates": {
+             "radec": [(image.ra, image.dec)],
+             "cone_search_radius": "1.2",
+             "cone_search_unit": "deg"
+         },
+         "catalogs": {
+             "Gaia_DR2": {
+                 "filter": {
+                     "parallax": {"$gt": 0.},
+                     "phot_g_mean_mag": {"$gt": 16.}  # only return stars
+                 },
+                 "projection": {
+                     "_id": 1,
+                     "ra": 1,
+                     "dec": 1
+                 }
+             }
+         },
+         "kwargs": {}
+    }
+    result = kowalski.query(q)
+    if result['status'] != 'done':
+        raise ValueError(f'Kowalski Error: {result}')
+
+    stars = result['result_data']['Gaia_DR2']
+    matchra = []
+    matchdec = []
+    for d in stars.values():
+        for dd in d:
+            matchra.append(dd['ra'])
+            matchdec.append(dd['dec'])
+
+    matchcoord = SkyCoord(matchra, matchdec, unit='deg')
+    catcoord = SkyCoord(catalog.data['X_WORLD'], catalog.data['Y_WORLD'], unit='deg')
+
+    idx, d2d, _ = catcoord.match_to_catalog_sky(matchcoord)
+    ind = d2d < 1 * u.arcsec
+    catok = catalog.data[ind]
+
     seeings = []
-    for row in data:
+    for row in catok:
         fwhm = row['FWHM_IMAGE']
         seeings.append(fwhm)
 
