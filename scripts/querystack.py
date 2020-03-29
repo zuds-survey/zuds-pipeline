8344789d2b6cb658334386dc4f475c8eebdd2c38
Danny Goldstein <dgold@berkeley.edu>
2020-03-29 12:39:18 -0400

    update querystack to solve stacks for stackedsubtractions
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- scripts/querystack.py
+++ scripts/querystack.py
@@ -3,81 +3,96 @@ from datetime import timedelta
 import pandas as pd
 import sys
 
-outfile = sys.argv[1]
 
-db.init_db()
 
 # set the stack window size
 STACK_WINDOW = 7.  # days
 STACK_INTERVAL = timedelta(days=STACK_WINDOW)
 
-# create the date table
-gs = db.sa.func.generate_series
-timetype = db.sa.DateTime(timezone=False)
-mindate = db.sa.cast('2017-01-03', timetype)
-maxdate = db.sa.cast(db.sa.func.now(), timetype)
+def solve_stacks(stackclass=db.ScienceCoadd):
 
-lcol = gs(mindate,
-          maxdate - STACK_INTERVAL,
-          STACK_INTERVAL).label('left')
+    # create the date table
+    gs = db.sa.func.generate_series
+    timetype = db.sa.DateTime(timezone=False)
+    mindate = db.sa.cast('2017-01-03', timetype)
+    maxdate = db.sa.cast(db.sa.func.now(), timetype)
 
-rcol = gs(mindate + STACK_INTERVAL,
-          maxdate,
-          STACK_INTERVAL).label('right')
+    lcol = gs(mindate,
+              maxdate - STACK_INTERVAL,
+              STACK_INTERVAL).label('left')
 
-daterange = db.DBSession().query(lcol, rcol).subquery()
+    rcol = gs(mindate + STACK_INTERVAL,
+              maxdate,
+              STACK_INTERVAL).label('right')
 
-target = db.sa.func.array_agg(db.ScienceImage.id).label('target')
-stacksize = db.sa.func.array_length(target, 1).label('stacksize')
-stackcond = stacksize >= 2
-jcond = db.sa.and_(db.ScienceImage.obsdate > daterange.c.left,
-                   db.ScienceImage.obsdate <= daterange.c.right)
+    daterange = db.DBSession().query(lcol, rcol).subquery()
 
-res = db.DBSession().query(db.ScienceImage.field,
-                           db.ScienceImage.ccdid,
-                           db.ScienceImage.qid,
-                           db.ScienceImage.fid,
-                           daterange.c.left, daterange.c.right,
-                           target).select_from(
-    db.sa.join(db.SingleEpochSubtraction, db.ScienceImage.__table__,
-               db.SingleEpochSubtraction.target_image_id ==
-               db.ScienceImage.id).join(
-        db.ReferenceImage.__table__, db.ReferenceImage.__table__.c.id ==
-                           db.SingleEpochSubtraction.reference_image_id
-    ).join(daterange, jcond)
-).filter(
-    db.ScienceImage.seeing < 4.,
-    db.ScienceImage.maglimit > 19.2,
-    db.ReferenceImage.version == 'zuds5',
-    db.ScienceImage.filefracday > 20200107000000
-).group_by(
-    db.ScienceImage.field,
-    db.ScienceImage.ccdid,
-    db.ScienceImage.qid,
-    db.ScienceImage.fid,
-    daterange.c.left, daterange.c.right
-).having(
-    stackcond
-).order_by(
-    stacksize.desc()
-).subquery()
+    target = db.sa.func.array_agg(db.ScienceImage.id).label('target')
+    stacksize = db.sa.func.array_length(target, 1).label('stacksize')
+    stackcond = stacksize >= 2
+    jcond = db.sa.and_(db.ScienceImage.obsdate > daterange.c.left,
+                       db.ScienceImage.obsdate <= daterange.c.right)
 
-excludecond = db.sa.and_(
-    db.ScienceCoadd.field == res.c.field,
-    db.ScienceCoadd.ccdid == res.c.ccdid,
-    db.ScienceCoadd.qid == res.c.qid,
-    db.ScienceCoadd.fid == res.c.fid,
-    db.ScienceCoadd.binleft == res.c.left,
-    db.ScienceCoadd.binright == res.c.right
-)
+    res = db.DBSession().query(db.ScienceImage.field,
+                               db.ScienceImage.ccdid,
+                               db.ScienceImage.qid,
+                               db.ScienceImage.fid,
+                               daterange.c.left, daterange.c.right,
+                               target).select_from(
+        db.sa.join(db.SingleEpochSubtraction, db.ScienceImage.__table__,
+                   db.SingleEpochSubtraction.target_image_id ==
+                   db.ScienceImage.id).join(
+            db.ReferenceImage.__table__, db.ReferenceImage.__table__.c.id ==
+                               db.SingleEpochSubtraction.reference_image_id
+        ).join(daterange, jcond)
+    ).filter(
+        db.ScienceImage.seeing < 4.,
+        db.ScienceImage.maglimit > 19.2,
+        db.ReferenceImage.version == 'zuds5',
+        db.ScienceImage.filefracday > 20200107000000
+    ).group_by(
+        db.ScienceImage.field,
+        db.ScienceImage.ccdid,
+        db.ScienceImage.qid,
+        db.ScienceImage.fid,
+        daterange.c.left, daterange.c.right
+    ).having(
+        stackcond
+    ).order_by(
+        stacksize.desc()
+    ).subquery()
+
+    excludecond = db.sa.and_(
+        stackclass.field == res.c.field,
+        stackclass.ccdid == res.c.ccdid,
+        stackclass.qid == res.c.qid,
+        stackclass.fid == res.c.fid,
+        stackclass.binleft == res.c.left,
+        stackclass.binright == res.c.right
+    )
 
 
-# get the coadds where there is no existing coadd already
-final = db.DBSession().query(res).outerjoin(
-    db.ScienceCoadd, excludecond
-).filter(
-    db.ScienceCoadd.id == None
-)
+    # get the coadds where there is no existing coadd already
+    final = db.DBSession().query(res).outerjoin(
+        stackclass, excludecond
+    ).filter(
+        stackclass.id == None
+    )
 
-result = pd.read_sql(final.statement, db.DBSession().get_bind())
-result.to_csv(outfile, index=False)
+    return final
+
+
+if __name__ == '__main__':
+
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('outfile')
+    parser.add_argument('--sub', action='store_true', default=False, dest='sub')
+    args = parser.parse_args()
+
+    outfile = args.outfile
+    stackclass = db.StackedSubtraction if args.sub else db.ScienceCoadd
+
+    final = solve_stacks(stackclass=stackclass)
+    result = pd.read_sql(final.statement, db.DBSession().get_bind())
+    result.to_csv(outfile, index=False)
