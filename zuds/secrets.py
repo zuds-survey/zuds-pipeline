fd5a278db12d7940d6e99f4d699934be306e8930
Danny Goldstein <dgold@berkeley.edu>
2020-04-19 07:51:18 -0400

    update secrets manager to use file on disk

--- zuds/secrets.py
+++ zuds/secrets.py
@@ -1,67 +1,62 @@
-# Use this code snippet in your app.
-# If you need more information about configurations or implementing the sample code, visit the AWS docs:
-# https://aws.amazon.com/developers/getting-started/python/
+import os
+import yaml
+import stat
+from pathlib import Path
 
-import boto3
-import base64
-from botocore.exceptions import ClientError
-import json
+__all__ = ['get_secret']
+
+class SecretsFilePermissionError(Exception):
+    pass
 
 class SecretManager(object):
+    """Class that manages access to secrets from the ZUDS configuration /
+    secrets file. Maintains a cache of secrets and implements the
+    `get_secret` function (as `__call__`) that is used elsewhere in
+    the codebase."""
 
-    def __init__(self):
-        self.cache = None
+    def __init__(self, debug=False):
+        """Read the configuration file and construct the secrets cache."""
+        
+        # See if an alternative path to the secrets file is specified
+        # using the environment variable 'ZUDS_CONFIG'
+        self.config_path = os.getenv('ZUDS_CONFIG')
+
+        # if no config_path is specified via an environment variable,
+        # use the default (~/.zuds)
+        if self.config_path is None:
+
+            # use the default
+            self.config_path = Path(os.getenv('HOME')) / '.zuds'
+
+        # check access to the file
+        bits = os.stat(self.config_path)
+        msgbase = '''Secrets file "%s" is accessible to %s, 
+                     but must disallow access to group and world 
+                     for security. Please run $chmod go-rwx %s 
+                     on the command line and retry.'''
+
+        # check for group access
+        if bits & stat.S_IRWXG > 0:
+            raise SecretsFilePermissionError(msg % (self.config_path,
+                                                    'group',
+                                                    self.config_path))
+
+        # check for world access
+        if bits & stat.S_IRWXO > 0:
+            raise SecretsFilePermissionError(msg % (self.config_path,
+                                                    'world',
+                                                    self.config_path))
+            
+        # load the secrets
+        self.cache = yaml.load(open(self.config_path, 'r'), 
+                               Loader=yaml.FullLoader)
 
     def __call__(self, key):
-        if self.cache is None:
-            self._update_cache()
-        return self.cache[key]
-
-    def _update_cache(self):
-
-        secret_name = "ztfsecrets"
-        region_name = "us-east-1"
-
-        # Create a Secrets Manager client
-        session = boto3.session.Session()
-        client = session.client(
-            service_name='secretsmanager',
-            region_name=region_name
-        )
-
-        # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-        # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        # We rethrow the exception by default.
-
-        try:
-            get_secret_value_response = client.get_secret_value(
-                SecretId=secret_name
-            )
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'DecryptionFailureException':
-                # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-                # An error occurred on the server side.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'InvalidParameterException':
-                # You provided an invalid value for a parameter.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'InvalidRequestException':
-                # You provided a parameter value that is not valid for the current state of the resource.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
-            elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-                # We can't find the resource that you asked for.
-                # Deal with the exception here, and/or rethrow at your discretion.
-                raise e
+        if key in self.cache:
+            value = self.cache[key]
+            if value is None:
+                raise ValueError(f'Secret value for key "{key}" is undefined.')
         else:
-            # Decrypts secret using the associated KMS CMK.
-            # Depending on whether the secret is a string or binary, one of these fields will be populated.
-            self.cache = json.loads(get_secret_value_response['SecretString'])
-
+            raise KeyError(f'Nonexistent secret requested: "{key}".')
 
 get_secret = SecretManager()
