2115a47fca51f4182c82f551672e44737e3575bd
Danny Goldstein <dgold@berkeley.edu>
2020-06-09 16:53:12 -0400

    update test
    Signed-off-by: Danny Goldstein <dgold@berkeley.edu>

--- zuds/tests/suite/test_config.py
+++ zuds/tests/suite/test_config.py
@@ -8,7 +8,7 @@ def test_secrets():
     assert zuds.get_secret('db_port') == 5432
     assert zuds.get_secret('db_username') == "admin"
     assert zuds.get_secret('db_name') == "zuds_test"
-    assert zuds.get_secret('db_password') is None
+    assert zuds.get_secret('db_password') == 'test'
 
 
 def test_update():
